3e916f0238ad980295b9d53fdc378a99cbe83dee
pax <coolwinding@gmail.com>
2016-12-28 11:43:55 +0800

    Support html slide show

--- xpandoc.sh
+++ xpandoc.sh
@@ -9,12 +9,13 @@ usage() {
 	cat <<EOF
 Usage: xpandoc [options] [markdown_files]...
   OPTIONS:
-      -c CORE_CSS               core css file path
-      -s SIDEBAR_CSS            sidebar css file path
-      -a ACCON_CSS              accondion css file path
+      -C CORE_CSS               core css file path
+      -S SIDEBAR_CSS            sidebar css file path
+      -A ACCON_CSS              accondion css file path
       -t toc style              0: no toc, 1: regular toc, 2: sidebar toc (default)
       -p                        pure html without any css
       -n                        output html without number sections
+      -s                        output html + javescript slide presentation
 
   By default, xpandoc converts pandoc's markdown files to
   html and pdf files embedded a sidebar.
@@ -26,21 +27,28 @@ EOF
 CORE_CSS=~/.pandoc/css/t.css
 SIDEBAR_CSS=~/.pandoc/css/t_sidebar.css
 ACCON_CSS=~/.pandoc/css/t_accondion.css
-TEMPLATE=t.html
+TEMPLATE="--template=t.html"
 
-TOC=--toc
+TOC="--toc"
 TOC_STYLE=2
-NUMBER_SECTIONS=--number-sections
+PURE=0
+NUMBER_SECTIONS="--number-sections"
+HIGHLIGHT_STYLE="--highlight-style=haddock"
+STANDALONE=""
+SELF_CONTAINED="--self-contained"
+TO="html"
+SLIDE_SHOW=0
 
-while getopts ":c:s:a:t:pn" OPTION
+while getopts ":C:S:A:t:pns" OPTION
 do
 	case $OPTION in
-	c ) CORE_CSS="$OPTARG";;
-	s ) SIDEBAR_CSS="$OPTARG";;
-	a ) ACCON_CSS="$OPTARG";;
+	C ) CORE_CSS="$OPTARG";;
+	S ) SIDEBAR_CSS="$OPTARG";;
+	A ) ACCON_CSS="$OPTARG";;
 	t ) TOC_STYLE="$OPTARG";;
-	p ) CORE_CSS="" && SIDEBAR_CSS="";;
+	p ) PURE=1;;
 	n ) NUMBER_SECTIONS="";;
+	s ) SLIDE_SHOW=1;;
 	* ) usage
 	esac
 done
@@ -63,11 +71,31 @@ else
 	CSS_A="-c "$ACCON_CSS""
 fi
 
-if [[ $TOC_STYLE -eq 0 ]]; then
+if [[ $TOC_STYLE == 0 ]]; then
+	CSS_S=""
 	TOC=""
+elif [[ $TOC_STYLE == 1 ]]; then
 	CSS_S=""
-elif [[ $TOC_STYLE -eq 1 ]]; then
+elif [[ $TOC_STYLE == 2 ]]; then
+	echo "--toc" > /dev/null
+else
+	echo "*** arg invalid" && usage
+fi
+
+if [[ $PURE == 1 ]]; then
+	CSS_C=""
 	CSS_S=""
+	CSS_A=""
+fi
+
+if [[ $SLIDE_SHOW == 1 ]]; then
+	TO="slidy"
+	CSS_C=""
+	CSS_S=""
+	CSS_A=""
+	TEMPLATE=""
+	STANDALONE="-s"
+	SELF_CONTAINED=""
 fi
 
 shift $(( $OPTIND - 1 ))
@@ -82,18 +110,21 @@ for arg in "$@"; do
 	# cmd: pandoc xxx -t html -o xxx.html
 	echo "pandoc \
 -f markdown+ignore_line_breaks \
--t html "$arg" -o "$name".html \
+-t "$TO" \
+"$arg" \
+-o "$name".html \
 "$CSS_C" \
 "$CSS_S" \
 "$CSS_A" \
 "$TOC" \
---template="$TEMPLATE" \
+"$TEMPLATE" \
 "$NUMBER_SECTIONS" \
---highlight-style=haddock \
---self-contained" | tee /tmp/xpandoc.cmd && sh < /tmp/xpandoc.cmd
+"$HIGHLIGHT_STYLE" \
+"$STANDALONE" \
+"$SELF_CONTAINED | tee /tmp/xpandoc.cmd && sh < /tmp/xpandoc.cmd
 
 	rm -f /tmp/xpandoc.cmd
-	if [[ "$?" -eq 0 ]]; then
+	if [[ "$?" == 0 ]]; then
 		echo ">>> output: create "$name".html"
 	else
 		echo ">>> output: create "$name".html failed!"
@@ -101,28 +132,15 @@ for arg in "$@"; do
 	fi
 	echo ""
 
-	if [[ -f /usr/bin/wkhtmltopdf ]]; then
-		# convert to pdf (via wkhtmltopdf or wkhtmltox if installed)
-		# cmd: wkhtmltopdf xxx.html xxx.pdf
-		echo "wkhtmltopdf --page-size A4 -T 15 -R 15 -B 15 -L 15 "$name".html "$name".pdf" | tee /tmp/xpandoc.cmd && sh < /tmp/xpandoc.cmd
-
-		rm -f /tmp/xpandoc.cmd
-		if [ "$?" -eq 0 ]; then
-			echo ">>> output: create "$name".pdf"
-		else
-			echo ">>> output: create "$name".pdf failed!"
-			exit 1
-		fi
-		echo ""
-	else
-		#echo "*** dependence: wkhtmltopdf not installed!"
-		if [[ -f /usr/bin/latex ]]; then
-			# convert to pdf (via LaTeX if installed)
-			# cmd: pandoc xxx -t latex -o xxx.pdf
-			echo "pandoc -t latex "$name".html -o "$name".pdf" | tee /tmp/xpandoc.cmd && sh < /tmp/xpandoc.cmd
+	# slide show doesn't need convert to pdf
+	if [[ ! $SLIDE_SHOW == 1 ]]; then
+		if [[ -f /usr/bin/wkhtmltopdf ]]; then
+			# convert to pdf (via wkhtmltopdf or wkhtmltox if installed)
+			# cmd: wkhtmltopdf xxx.html xxx.pdf
+			echo "wkhtmltopdf --page-size A4 -T 15 -R 15 -B 15 -L 15 "$name".html "$name".pdf" | tee /tmp/xpandoc.cmd && sh < /tmp/xpandoc.cmd
 
 			rm -f /tmp/xpandoc.cmd
-			if [ "$?" -eq 0 ]; then
+			if [ "$?" == 0 ]; then
 				echo ">>> output: create "$name".pdf"
 			else
 				echo ">>> output: create "$name".pdf failed!"
@@ -130,8 +148,24 @@ for arg in "$@"; do
 			fi
 			echo ""
 		else
-			#echo "*** dependence: latex not installed!"
-			echo "*** dependence: install wkhtmltopdf or latex to support convert to PDF."
+			#echo "*** dependence: wkhtmltopdf not installed!"
+			if [[ -f /usr/bin/latex ]]; then
+				# convert to pdf (via LaTeX if installed)
+				# cmd: pandoc xxx -t latex -o xxx.pdf
+				echo "pandoc -t latex "$name".html -o "$name".pdf" | tee /tmp/xpandoc.cmd && sh < /tmp/xpandoc.cmd
+
+				rm -f /tmp/xpandoc.cmd
+				if [ "$?" == 0 ]; then
+					echo ">>> output: create "$name".pdf"
+				else
+					echo ">>> output: create "$name".pdf failed!"
+					exit 1
+				fi
+				echo ""
+			else
+				#echo "*** dependence: latex not installed!"
+				echo "*** dependence: install wkhtmltopdf or latex to support convert to PDF."
+			fi
 		fi
 	fi
 
